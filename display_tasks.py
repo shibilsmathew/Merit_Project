38685f844821d82fbea08055c7327a5a38ecacb1
greeshmavamsi <144332589+greeshmavamsi@users.noreply.github.com>
2024-05-09 23:03:18 +0530

    Updated display_tasks.py

--- display_tasks.py
+++ display_tasks.py
@@ -4,5 +4,5 @@ class TaskManager:
         tasks_data = []
         for index, task in enumerate(self.tasks, 1):
             tasks_data.append([index, task.title, task.status, task.time_taken, task.expected_finish])
-        headers = ["", "Task", "Status", "Time Taken", "Expected Finish Time"]
+        headers = ["", "Task", "Status", "Time Taken", "Finished at"]
         print(tabulate(tasks_data, headers=headers, tablefmt="grid"))
